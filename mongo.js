1e535272b5cd99ab2c15a2aeea62696c34281d58
ashirafumiiro <ashirafumiiro@gmail.com>
2020-09-21 18:56:39 +0300

    unit testing

--- mongo.js
+++ mongo.js
@@ -1,33 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = 
-    `mongodb+srv://fullstack:${password}@cluster0.gezd5.mongodb.net/note-app?retryWrites=true&w=majority`;
+const url =
+    `mongodb+srv://fullstack:${password}@cluster0.gezd5.mongodb.net/note-app?retryWrites=true&w=majority`
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const noteSchema = mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+  content: String,
+  date: Date,
+  important: Boolean,
 })
 
 noteSchema.set('toJSON',{
-    transform: (document, returnedObject)=>{
-        returnedObject.id = returnedObject._id.toString();
-        
-        delete returnedObject._id;
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 // const note = new Note({
 //     content: 'GET and POST are the most important methods of HTTP protocol',
@@ -41,8 +41,8 @@ const Note = mongoose.model('Note', noteSchema);
 // })
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
---
+++ tests/notes_api.test.js
@@ -0,0 +1,52 @@
+const app = require('../app')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const Note = require('../models/note')
+
+const api = supertest(app)
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    date: new Date(),
+    important: true,
+  },
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  afterAll(() => {
+    mongoose.connection.close()
+  })
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body).toHaveLength(2)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body[0].content).toBe('HTML is easy')
+})
